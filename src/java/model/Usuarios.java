937bdf0ea8fc1b1256daeb47b36aa1667cc97980
Alessandro <alessandrocosta103@gmail.com>
2023-02-01 21:51:42 -0300

    cria classes Login e Usuarios

---
+++ src/java/model/Usuarios.java
@@ -0,0 +1,30 @@
+
+package model;
+
+public class Usuarios {
+    
+    private String login,senha;
+    
+    public Usuarios(){
+        
+    }
+    
+    public String getLogin(){
+        return login;
+    }
+    
+    public void setLogin(String login){
+        this.login = login;
+    }
+    
+    public String getSenha(){
+        return senha;
+    }
+    
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    
+    
+    
+}
--- src/java/servlet/Servlet.java
+++ src/java/servlet/Servlet.java
@@ -14,17 +14,7 @@ public class Servlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
     @Override
